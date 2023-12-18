5d181885e78c331a0f2647d664788c3bcb00599e
Johnson <johnsonta87@gmail.com>
2023-12-18 14:02:40 -0500

    add query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 import { PortfolioList } from "@/components";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-
-  return <PortfolioList variant="default" />;
+  return <PortfolioList variant="default" category="Product design" />;
 };
 
 export default Home;
--- src/components/PortfolioList/PortfolioList.tsx
+++ src/components/PortfolioList/PortfolioList.tsx
@@ -1,72 +1,86 @@
-"use client"
-import { useGetAllPortfoliosQuery as GetAllPortfolio, Portfolio } from "@/__generated__/graphql";
-import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+"use client";
+import { useGetPortfoliosByCategoryQuery as GetPortfoliosByCategory } from "@/__generated__/graphql";
+import { Box, Grid, GridItem, Spinner, Text } from "@chakra-ui/react";
 import BaseImageCaption from "../Image/BaseImageCaption";
-import { Spinner } from '@chakra-ui/react'
 
 type Props = {
+  category: "Product design" | "Branding design";
   variant: "default" | "twoColumns";
 };
 
-export function PortfolioList({ variant }: Props) {
-  const { loading, data } = GetAllPortfolio();
+export function PortfolioList({ category, variant }: Props) {
+  const { loading, data } = GetPortfoliosByCategory(category);
   const portfolios = data?.portfolios;
 
-  if(loading) return <Spinner size='xl' />;
+  if (loading) return <Spinner size="xl" />;
 
+  if (variant === "twoColumns")
+    return (
+      <Box>
+        <Text fontSize="4xl" mb="8">
+          Portfolio
+        </Text>
 
-  if(variant === "twoColumns") return (
-    <Box>
-      <Text fontSize="4xl" mb="8">Portfolio</Text>
-
-    <Grid
-    gridTemplateColumns={{ base: "1fr", md: "50% 1fr"}}
-    gap={{ base: "6", md: "12" }}>
-
-{portfolios &&
-        portfolios.map((p, index: number) => (
-          <GridItem key={index}>
-            <BaseImageCaption
-              src={p.showcaseImage?.url || ""}
-              title={p.title || ""}
-              caption={p.title || ""}
-              year={p.year || ""}
-              category={p.portfolioCategory?.name || ""}
-            />
-          </GridItem>
-        ))}
-    </Grid>
-    </Box>
-  )
+        <Grid
+          gridTemplateColumns={{ base: "1fr", md: "50% 1fr" }}
+          gap={{ base: "6", md: "12" }}
+        >
+          {portfolios &&
+            portfolios.map((p, index: number) => (
+              <GridItem key={index}>
+                <BaseImageCaption
+                  src={p.showcaseImage?.url || ""}
+                  title={p.title || ""}
+                  caption={p.title || ""}
+                  year={p.year || ""}
+                  category={p.portfolioCategory?.name || ""}
+                />
+              </GridItem>
+            ))}
+        </Grid>
+      </Box>
+    );
 
   return (
     <Box>
-
-    <Grid
-      templateAreas={`"side main"`}
-      gridTemplateColumns={"260px 1fr"}
-    gap={{ base: "6", md: "12" }}>
-
-<GridItem area={"side"}>
-      <Text fontSize="4xl" mb="8">Portfolio</Text>
-      </GridItem>
-<GridItem area={"main"}>
-
-<Grid gap={{ base: "6", md: "12" }}>
-{portfolios &&
-        portfolios.map((p, index: number) => (
-          <GridItem key={index}>
-            <BaseImageCaption
-              src={p.showcaseImage?.url || ""}
-              title={p.title || ""}
-              caption={p.title || ""}
-              year={p.year || ""}
-            />
-          </GridItem>
-        ))}
-        </Grid>
+      <Grid
+        templateAreas={`"side main"`}
+        gridTemplateColumns={"260px 1fr"}
+        gap={{ base: "6", md: "12" }}
+      >
+        <GridItem area={"side"}>
+          <Text fontSize="4xl" mb="8">
+            Portfolio
+          </Text>
         </GridItem>
-    </Grid>
+        <GridItem area={"main"}>
+          <Grid gap={{ base: "6", md: "12" }} mb={6}>
+            {portfolios && (
+              <GridItem>
+                <BaseImageCaption
+                  src={portfolios[0].showcaseImage?.url || ""}
+                  title={portfolios[0].title || ""}
+                  caption={portfolios[0].title || ""}
+                  year={portfolios[0].year || ""}
+                />
+              </GridItem>
+            )}
+          </Grid>
+          <Grid gridTemplateColumns={"50% 1fr"} gap={{ base: "6", md: "12" }}>
+            {portfolios &&
+              portfolios.slice(1).map((p, index: number) => (
+                <GridItem key={index}>
+                  <BaseImageCaption
+                    src={p.showcaseImage?.url || ""}
+                    title={p.title || ""}
+                    caption={p.title || ""}
+                    year={p.year || ""}
+                  />
+                </GridItem>
+              ))}
+          </Grid>
+        </GridItem>
+      </Grid>
     </Box>
   );
 }
